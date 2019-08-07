5197c5ee98ab3291af956539fb762e145b24b1ca
MonkeyDwwl <myers_wang@163.com>
2019-08-07 10:12:27 +0800

    add quickSort

---
+++ quicksort.js
@@ -0,0 +1,28 @@
+/*
+* 快速排序（非原地）
+* 思想： 1. 从数组中找到一个基准数；
+* 2. 以这个数为基准进行分区，将不小于它的放到右边，将不大于它的全放到左边
+* 3. 再分别对左右两个子数组重复第二步，直到各个子数组只有一个数
+*/
+function quickSort(arr) {
+    if (arr.length <= 1)
+        return arr;
+    var q = arr.splice(0, 1);
+    console.log('q>>>>>', q);
+    var leftArr = [];
+    var rightArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < q) {
+            leftArr.push(arr[i]);
+        }
+        else {
+            rightArr.push(arr[i]);
+        }
+    }
+    return quickSort(leftArr).concat(q, quickSort(rightArr));
+}
+// 测试
+var testArr = [23, 10, 1, 92, 77, 45, 62, 5, 92, 100];
+console.log('testArr>>>', testArr);
+var afterSortArr = quickSort(testArr);
+console.log('afterSortArr>>>>>>', afterSortArr);
